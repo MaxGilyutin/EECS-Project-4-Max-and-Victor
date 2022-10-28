0faf01d14708bdbf06056b04890379b916448a6d
vjdiaso <113306393+vjdiaso@users.noreply.github.com>
2022-10-28 13:27:36 -0400

    Update Color.cpp

--- Color.cpp
+++ Color.cpp
@@ -15,7 +15,73 @@
 
 // TODO: implement first checkRange, then two constructors, setRed, getRed,
 //       setGreen, getGreen, setBlue, getBlue, read, write.
+Color::Color()
+{
+	red = 0;
+	green = 0;
+	blue = 0;
+}
 
+Color::Color(int redVal, int greenVal, int blueVal)
+{
+	red = checkRange(redVal);
+	green = checkRange(greenVal);
+	blue = checkRange(blueVal);
+}
+
+void Color::setRed(int redVal)
+{
+	red = checkRange(redVal);
+}
+
+int Color::getRed()
+{
+	return red;
+}
+
+void Color::setGreen(int greenVal)
+{
+	green = checkRange(greenVal);
+}
+
+int Color::getGreen()
+{
+	return green;
+}
+
+void Color::setBlue(int blueVal)
+{
+	blue = checkRange(blueVal);
+}
+
+int Color::getBlue()
+{
+	return blue;
+}
+
+void Color::read(istream& ins)
+{
+	ins >> red >> green >> blue;
+}
+
+void Color::write(ostream& outs)
+{
+	outs << red << " " << green << " " << blue;
+}
+
+int Color::checkRange(int val)
+{
+	if (val >= 0 && val <= 255) {
+		return val;
+	}
+	else if (val < 0) {
+		return 0;
+	}
+	else 
+	{
+		return 255;
+	}
+}
 
 
 // Your code goes above this line.
