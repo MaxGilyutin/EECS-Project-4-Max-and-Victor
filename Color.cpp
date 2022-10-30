135ce8f9f990200cf5abdd63d920821fc61f165d
vjdiaso <113306393+vjdiaso@users.noreply.github.com>
2022-10-30 15:47:29 -0400

    Update Color.cpp

--- Color.cpp
+++ Color.cpp
@@ -108,9 +108,9 @@ int Color::getBlue()
 void Color::read(istream& ins)
 {
 	ins >> red >> green >> blue;
-	checkRange(red);
-	checkRange(green);
-	checkRange(blue);
+	red = checkRange(red);
+	green = checkRange(green);
+	blue = checkRange(blue);
 }
 
 void Color::write(ostream& outs)
